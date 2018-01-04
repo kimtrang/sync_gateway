d515b51392bb7858532fd80fa0ad4effae21158a
Traun Leyden <tleyden@couchbase.com>
2018-01-04 15:00:01 -0800

    Finish TestContinousChangesSubscription() and add stubs

--- rest/blip_api_test.go
+++ rest/blip_api_test.go
@@ -2,22 +2,15 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"strings"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/couchbase/go-blip"
 	"github.com/couchbaselabs/go.assert"
 )
-
-// What's missing:
-// - Continuous changes feed
-// - Getting and setting a checkpoint
-// - Getting and setting attachments
-// - No-conflicts mode replication (proposeChanges)
-// - Unsolicited rev request
-// - Connect to public port with authentication
 
 // This test performs the following steps against the Sync Gateway passive blip replicator:
 //
@@ -47,7 +40,6 @@ func TestBlipPushRevisionInspectChanges(t *testing.T) {
 	assertNoError(t, err, "Error reading changes response body")
 	err = json.Unmarshal(body, &changeList)
 	assertNoError(t, err, "Error unmarshalling response body")
-	log.Printf("changes response body: %s", body)
 	assert.True(t, len(changeList) == 1) // Should be 1 row, corresponding to the single doc that was queried in changes
 	changeRow := changeList[0]
 	assert.True(t, len(changeRow) == 0) // Should be empty, meaning the server is saying it doesn't have the revision yet
@@ -66,7 +58,6 @@ func TestBlipPushRevisionInspectChanges(t *testing.T) {
 	assert.Equals(t, revResponse.SerialNumber(), revRequest.SerialNumber())
 	body, err = revResponse.Body()
 	assertNoError(t, err, "Error unmarshalling response body")
-	log.Printf("rev response body: %s", body)
 
 	// Call changes with a hypothetical new revision, assert that it returns last pushed revision
 	var changeList2 [][]interface{}
@@ -79,7 +70,6 @@ func TestBlipPushRevisionInspectChanges(t *testing.T) {
 	assert.Equals(t, changesResponse2.SerialNumber(), changesRequest2.SerialNumber())
 	body2, err := changesResponse2.Body()
 	assertNoError(t, err, "Error reading changes response body")
-	log.Printf("changes2 response body: %s", body2)
 	err = json.Unmarshal(body2, &changeList2)
 	assertNoError(t, err, "Error unmarshalling response body")
 	assert.True(t, len(changeList2) == 1) // Should be 1 row, corresponding to the single doc that was queried in changes
@@ -96,16 +86,31 @@ func TestBlipPushRevisionInspectChanges(t *testing.T) {
 		log.Printf("got changes message: %+v", request)
 		body, err := request.Body()
 		log.Printf("changes body: %v, err: %v", string(body), err)
-		// Expected changes body: [[1,"foo","1-abc"]]
-		changeListReceived := [][]interface{}{}
-		err = json.Unmarshal(body, &changeListReceived)
-		assertNoError(t, err, "Error unmarshalling changes recevied")
-		assert.True(t, len(changeListReceived) == 1)
-		change := changeListReceived[0] // [1,"foo","1-abc"]
-		assert.True(t, len(change) == 3)
-		assert.Equals(t, change[0].(float64), float64(1)) // Original sequence sent in pushed rev
-		assert.Equals(t, change[1], "foo")                // Doc id of pushed rev
-		assert.Equals(t, change[2], "1-abc")              // Rev id of pushed rev
+
+		if string(body) != "null" {
+
+			// Expected changes body: [[1,"foo","1-abc"]]
+			changeListReceived := [][]interface{}{}
+			err = json.Unmarshal(body, &changeListReceived)
+			assertNoError(t, err, "Error unmarshalling changes recevied")
+			assert.True(t, len(changeListReceived) == 1)
+			change := changeListReceived[0] // [1,"foo","1-abc"]
+			assert.True(t, len(change) == 3)
+			assert.Equals(t, change[0].(float64), float64(1)) // Original sequence sent in pushed rev
+			assert.Equals(t, change[1], "foo")                // Doc id of pushed rev
+			assert.Equals(t, change[2], "1-abc")              // Rev id of pushed rev
+
+		}
+
+		if !request.NoReply() {
+			// Send an empty response to avoid the Sync: Invalid response to 'changes' message
+			response := request.Response()
+			emptyResponseVal := []interface{}{}
+			emptyResponseValBytes, err := json.Marshal(emptyResponseVal)
+			assertNoError(t, err, "Error marshalling response")
+			response.SetBody(emptyResponseValBytes)
+		}
+
 		receviedChangesRequestWg.Done()
 
 	}
@@ -120,6 +125,10 @@ func TestBlipPushRevisionInspectChanges(t *testing.T) {
 	subChangesResponse := subChangesRequest.Response()
 	assert.Equals(t, subChangesResponse.SerialNumber(), subChangesRequest.SerialNumber())
 
+	// Also expect the "changes" profile handler above to be called back again with an empty request that
+	// will be ignored since body will be "null"
+	receviedChangesRequestWg.Add(1)
+
 	// Wait until we got the expected callback on the "changes" profile handler
 	receviedChangesRequestWg.Wait()
 
@@ -133,12 +142,62 @@ func TestContinousChangesSubscription(t *testing.T) {
 	bt := CreateBlipTester(t)
 	defer bt.Close()
 
+	// Counter/Waitgroup to help ensure that all callbacks on continuous changes handler are received
+	receviedChangesWg := sync.WaitGroup{}
+
 	// When this test sends subChanges, Sync Gateway will send a changes request that must be handled
+	lastReceivedSeq := float64(0)
 	bt.blipContext.HandlerForProfile["changes"] = func(request *blip.Message) {
 
 		log.Printf("got changes message: %+v", request)
 
+		body, err := request.Body()
+		log.Printf("changes body: %v, err: %v", string(body), err)
+
+		if string(body) != "null" {
+
+			// Expected changes body: [[1,"foo","1-abc"]]
+			changeListReceived := [][]interface{}{}
+			err = json.Unmarshal(body, &changeListReceived)
+			assertNoError(t, err, "Error unmarshalling changes recevied")
+
+			log.Printf("changeListReceived: %v", changeListReceived)
+			assert.True(t, len(changeListReceived) == 1)
+
+			// Assuming that the callback receives a single change at a time, since
+			// docs are being inserted one at a time
+			change := changeListReceived[0] // [1,"foo","1-abc"]
+			assert.True(t, len(change) == 3)
+
+			// Make sure sequence numbers are monotonically increasing
+			receivedSeq := change[0].(float64)
+			assert.True(t, receivedSeq > lastReceivedSeq)
+			lastReceivedSeq = receivedSeq
+
+			// Verify doc id and rev id have expected vals
+			docId := change[1].(string)
+			assert.True(t, strings.HasPrefix(docId, "foo"))
+			assert.Equals(t, change[2], "1-abc") // Rev id of pushed rev
+
+		}
+
+		if !request.NoReply() {
+			// Send an empty response to avoid the Sync: Invalid response to 'changes' message
+			response := request.Response()
+			emptyResponseVal := []interface{}{}
+			emptyResponseValBytes, err := json.Marshal(emptyResponseVal)
+			assertNoError(t, err, "Error marshalling response")
+			response.SetBody(emptyResponseValBytes)
+		}
+
+		receviedChangesWg.Done()
+
 	}
+
+	// Increment waitgroup since just the act of subscribing to continuous changes will cause
+	// the callback changes handler to be invoked with an initial change w/ empty body, signaling that
+	// all of the changes have been sent (eg, there are no changes to send)
+	receviedChangesWg.Add(1)
 
 	// Send subChanges to subscribe to changes, which will cause the "changes" profile handler above to be called back
 	subChangesRequest := blip.NewRequest()
@@ -149,7 +208,28 @@ func TestContinousChangesSubscription(t *testing.T) {
 	subChangesResponse := subChangesRequest.Response()
 	assert.Equals(t, subChangesResponse.SerialNumber(), subChangesRequest.SerialNumber())
 
-	time.Sleep(time.Second * 5)
+	for i := 1; i < 5; i++ {
+
+		// Add a change: Send an unsolicited doc revision in a rev request
+		receviedChangesWg.Add(1)
+		revRequest := blip.NewRequest()
+		revRequest.SetProfile("rev")
+		revRequest.Properties["id"] = fmt.Sprintf("foo-%d", i)
+		revRequest.Properties["rev"] = "1-abc"
+		revRequest.Properties["deleted"] = "false"
+		revRequest.Properties["sequence"] = fmt.Sprintf("%d", i)
+		revRequest.SetBody([]byte(`{"key": "val"}`))
+		sent = bt.sender.Send(revRequest)
+		assert.True(t, sent)
+		revResponse := revRequest.Response()
+		assert.Equals(t, revResponse.SerialNumber(), revRequest.SerialNumber())
+		body, err := revResponse.Body()
+		assertNoError(t, err, "Error unmarshalling response body")
+		log.Printf("rev response body: %s", body)
+
+	}
+
+	receviedChangesWg.Wait()
 
 }
 
@@ -158,6 +238,28 @@ func TestConcurrentChangesSubscriptions(t *testing.T) {
 
 }
 
+// Create a continous changes subscription that has docs in multiple channels, and make sure
+// all docs are received
 func TestMultiChannelContinousChangesSubscription(t *testing.T) {
+
+}
+
+// Test setting and getting checkpoints
+func TestCheckpoints(t *testing.T) {
+
+}
+
+// Test no-conflicts mode replication (proposeChanges endpoint)
+func TestNoConflictsModeReplication(t *testing.T) {
+
+}
+
+// Test adding / retrieving attachments
+func TestAttachments(t *testing.T) {
+
+}
+
+// Connect to public port with authentication
+func TestPublicPortAuthentication(t *testing.T) {
 
 }
